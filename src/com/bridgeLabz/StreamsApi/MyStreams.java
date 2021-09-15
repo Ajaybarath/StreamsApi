72e2bff0c098e10b051a0bd7cf4b23c009a8a35c
ajaybarath <ajaybarath3@gmail.com>
2021-09-15 07:34:31 +0530

    ADDED: streams to filter the even numbers and store in the list

--- src/com/bridgeLabz/StreamsApi/MyStreams.java
+++ src/com/bridgeLabz/StreamsApi/MyStreams.java
@@ -3,6 +3,7 @@ package com.bridgeLabz.StreamsApi;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -25,5 +26,14 @@ public class MyStreams {
 		
 		List<Double> doubleList = myList.stream().map(toDoubleFunction).collect(Collectors.toList());
 		System.out.println(doubleList);
+		
+		
+		Predicate<Integer> isEvenFunction = n -> n%2 == 0;
+		myList.stream().forEach(n -> System.out.println(isEvenFunction.test(n)));
+
+		List<Integer> evenList = myList.stream().filter(isEvenFunction).collect(Collectors.toList());
+		
+		System.out.println(evenList);
+		
 	}
 }
