983fb8253c15be25f4155921581231565d156e85
ajaybarath <ajaybarath3@gmail.com>
2021-09-15 07:26:18 +0530

    ADDED: stream to iterate and print all the element

--- src/com/bridgeLabz/StreamsApi/MyStreams.java
+++ src/com/bridgeLabz/StreamsApi/MyStreams.java
@@ -1,5 +1,19 @@
 package com.bridgeLabz.StreamsApi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyStreams {
 
+	
+	public static void main(String args[]) {
+
+		List<Integer> myList = new ArrayList<>();
+		myList.add(5);
+		myList.add(6);
+		myList.add(8);
+
+		// iterate to show each element of stream
+		myList.stream().forEach(n -> System.out.println(n));
+	}
 }
